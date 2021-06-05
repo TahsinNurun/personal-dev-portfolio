e3a95aa743adc7819447948e4545ce5d288e8c58
Tahsin Nurun <nuruntahsin@gmail.com>
2021-06-06 03:23:56 +0600

    008

--- src/Components/BlogDetail/BlogDetail.js
+++ src/Components/BlogDetail/BlogDetail.js
@@ -3,8 +3,8 @@ import React from 'react';
 const BlogDetail = ({ blog }) => {
     return (
 
-            <div className="card ms-auto mx-3 my-3 shadow-lg" style={{ height: '35rem', width: '21rem'}}>
-                <img className="card-img-top mx-auto d-block mt-3 p-1" src={blog.image} alt="Card image cap" style={{width: '20rem', height: '10rem'}} />
+            <div className="card mb-3 container w-25 mt-3 shadow-lg" style={{ height: '35rem', width: '21rem'}}>
+                <img className="card-img-top mx-auto d-block mt-3 p-1" src={blog.image} alt="Card image cap" style={{width: '30vh', height: '15vh'}} />
                 <div className="card-body text-center">
                     <h5 className="card-title">{blog.title}</h5>
                     <p className="card-text">{blog.description}</p>
